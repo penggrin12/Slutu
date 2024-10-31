bd5e80bb1fa8370b484f786420379435e4439674
penggrin12 <miner.sidor@gmail.com>
2024-10-31 17:50:57 +0300

    utils.get_file, utils.get_args_raw, utils.get_args

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,9 @@
 import logging
 from pathlib import Path
+from typing import List, Union
 
 import checksumdir
-from telethon.types import Message
+from telethon.types import Message, File
 
 
 def get_logger(cls: object) -> logging.Logger:
@@ -22,3 +23,24 @@ def get_app_hash() -> str:
 def get_plugins_hash() -> str:
     return checksumdir.dirhash(Path(".") / Path("plugins"), excluded_extensions=["pyc"])
 
+
+async def get_file(message: Message) -> Union[File, None]:
+    if message.file:
+        return message.file
+    if message.replied_message_id:
+        replied_to: Message = await message.get_replied_message()
+        return await get_file(replied_to)  # NOTICE: possible to be a reaaallyyy long recursion
+    return None  # explicit None
+
+
+def get_args_raw(message: Message) -> str:
+    if " " not in message.text_html:
+        return ""
+    return message.text_html.split(" ", 1)[-1]
+
+
+def get_args(message: Message) -> List[str]:
+    args_raw: str = get_args_raw(message)
+    if not args_raw:
+        return []
+    return args_raw.split(" ")
